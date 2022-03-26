f6170e34cc90bee03ae3857da249c8596bec7073
Everton Rodrigues Silva <evertosilves@gmail.com>
2022-03-26 13:04:51 -0300

    trabalho de lógica
    em js

---
+++ Nota_Enem/nota_enem.js
@@ -0,0 +1,10 @@
+let nome = null
+let red = null
+let mat = null
+let port = null
+let nat = null
+let hum = null
+
+
+let comando = prompt(`Digite se quer "ver" ou "adicionar" uma nota ou "fechar" o programa: `).toLowerCase()
+
