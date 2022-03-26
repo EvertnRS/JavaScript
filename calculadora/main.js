6b4667f1e82ddd6c00d8d03e4920b30ee6914e4f
Everton Rodrigues Silva <evertosilves@gmail.com>
2022-03-26 10:53:38 -0300

    Atualização do projeto

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,30 +1,52 @@
-var txtn7 = document.getElementById("n7")
-var n7 = Number(txtn7.value)
-var txtn8 = document.getElementById("n8")
-var n8 = Number(txtn8.value)
-var txtn9 = document.getElementById("n9")
-var n9 = Number(txtn9.value)
-var txtn4 = document.getElementById("n4")
-var n4 = Number(txtn4.value)
-var txtn5 = document.getElementById("n5")
-var n5 = Number(txtn5.value)
-var txtn6 = document.getElementById("n6")
-var n6 = Number(txtn6.value)
-var txtn1 = document.getElementById("n1")
-var n1 = Number(txtn1.value)
-var txtn2 = document.getElementById("n2")
-var n2 = Number(txtn2.value)
-var txtn3 = document.getElementById("n3")
-var n3 = Number(txtn3.value)
-var txtn0 = document.getElementById("n0")
-var n0 = Number(txtn0.value)
-var vis = document.getElementById("vis")
+var vis = document.getElementById("vis").innerHTML
+function aparecer(n){ 
+     var num = document.getElementById("vis").innerHTML
+     document.getElementById("vis").innerHTML = num + n
+     if(n == "."){
+          document.getElementById("vis").innerHTML
+          document.getElementById("vis").innerHTML = num + ","
+     } else if (n == "*"){
+          document.getElementById("vis").innerHTML
+          document.getElementById("vis").innerHTML = num + "x"
+     } else if (n == "**"){
+          document.getElementById("vis").innerHTML
+          document.getElementById("vis").innerHTML = num + "^"
+     }
+     
+}
+function c(){
+     var apagar = document.getElementById("vis").innerHTML
+     document.getElementById("vis").innerHTML = ""
+}
+function del(){
+     var del = document.getElementById("vis").innerHTML
+     document.getElementById("vis").innerHTML = del.substring(0, del.length - 1)
+}
 
-function aparecer(n){
-    vis.innerHTML = `${n}`
-    var num = n
-    
+function resultado(res){
+     if(vis.length == 0) {
+          alert("Erro!!!")
+     } else {
+          if(vis.indexOf(",")) {
+               let ponto = document.getElementById("vis").innerHTML
+               ponto = ponto.replace(",", ".")
+               let resul = document.getElementById("vis").innerHTML
+               document.getElementById("vis").innerHTML = eval(ponto)
+               let virg = document.getElementById("vis").innerHTML
+               virg = virg.replace(".", ",")
+               document.getElementById("vis").innerHTML = virg
+          } else if(vis.indexOf("x")) {
+               let ast = document.getElementById("vis").innerHTML
+               ast = ast.replace("x", "*")
+               let resul = document.getElementById("vis").innerHTML
+               document.getElementById("vis").innerHTML = eval(ast)
+          } else if(vis.indexOf("^")){
+               let sup = document.getElementById("vis").innerHTML
+               sup = sup.replace("^", "**")
+               let resul = document.getElementById("vis").innerHTML
+               document.getElementById("vis").innerHTML = eval(sup)
+          }
+     }
+     
 }
-function virgula() {
-    alert(n)
-}
+
